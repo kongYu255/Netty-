bfca6ad2d6d3b80114326a44355b98659a64fcd4
yuyong <lyxyy20>
2020-09-14 18:37:12 +0800

    1

--- src/main/java/com/yuy/netty/server/FileUploadServer.java
+++ src/main/java/com/yuy/netty/server/FileUploadServer.java
@@ -8,7 +8,7 @@ import io.netty.handler.codec.serialization.ClassResolvers;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 import io.netty.handler.codec.serialization.ObjectEncoder;
 
-public class Server {
+public class FileUploadServer {
 
 
     public static void main(String[] args) throws InterruptedException {
